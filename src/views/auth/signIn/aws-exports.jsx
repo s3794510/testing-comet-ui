89f29cf93009300e28e27767792702c40111c83f
Tung <s3794510@rmit.edu.vn>
2023-08-12 02:20:55 +0700

    testing: react template

--- src/views/auth/signIn/aws-exports.jsx
+++
@@ -1,9 +0,0 @@
-const awsmobile = {
-  Auth: {
-    region: "YOUR_COGNITO_REGION",
-    userPoolId: "YOUR_COGNITO_USER_POOL_ID",
-    userPoolWebClientId: "YOUR_COGNITO_APP_CLIENT_ID",
-  },
-};
-
-export default awsmobile;
